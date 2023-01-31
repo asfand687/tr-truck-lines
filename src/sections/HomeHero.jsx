c1f05e5432ddfd6f81515a9d5688a1efa327ee18
asfand687 <asfandyar687@gmail.com>
2023-02-01 00:50:59 +0500

    fixed height for mobile screen hero section

--- src/sections/HomeHero.jsx
+++ src/sections/HomeHero.jsx
@@ -4,15 +4,15 @@ import BGVideo from '../assets/bg-video.mp4'
 
 const HomeHero = () => {
   return (
-    <section className="h-[calc(100vh-65px)]">
+    <section className="h-screen md:h-[calc(100vh-65px)]">
       <div className="relative">
         <div className="fixed left-10 bottom-0 bg-yellow-400 rounded-full z-50 w-24 h-24 flex justify-center items-center animate-pulse">
           <AiTwotonePhone className="text-5xl" />
         </div>
-        <div className="absolute w-full h-[calc(100vh-65px)] z-20 flex justify-center items-center text-white gap-x-16">
-          <article className="flex">
+        <div className="absolute w-full  h-screen md:h-[calc(100vh-65px)] z-20 flex flex-col md:flex-row justify-center items-center text-white gap-x-16">
+          <article className="mt-[200px] px-4 pb-10 md:mt-0 md:pb-0">
             <div data-aos="fade-right" className="w-full text-center lg:text-left lg:max-w-[450px]">
-              <div className="h-[6px] w-36 bg-yellow-600 mb-6"></div>
+              <div className="mx-auto md:mx-0 h-[6px] w-36 bg-yellow-600 mb-6"></div>
               <h2 className="text-4xl lg:text-7xl font-bold lg:max-w-[500px]">Freight Dispatch Service</h2>
               <p className="py-4">Full dispatch services for trucks. We provide high quality dispatching services with the best industry standards and additional realted services.</p>
               <button className="bg-black text-yellow-600 rounded w-full max-w-[280px] py-3 font-medium">
@@ -43,10 +43,10 @@ const HomeHero = () => {
             </form>
         </article>
         </div>
-        <video className="w-full h-[calc(100vh-65px)] object-cover" autoPlay playsInline muted loop>
+        <video className="w-full  h-screen md:h-[calc(100vh-65px)] object-cover" autoPlay playsInline muted loop>
           <source src={BGVideo} type="video/mp4" />
         </video>
-        <div className="absolute z-10 top-0 left-0 h-[calc(100vh-65px)] w-full bg-[#0000007a]"></div>
+        <div className="absolute z-10 top-0 left-0  h-screen md:h-[calc(100vh-65px)] w-full bg-[#0000007a]"></div>
         <div className="flex flex-col-reverse md:flex-row items-center px-4 py-6 md:py-20 w-full max-w-7xl mx-auto">
 
         </div>
