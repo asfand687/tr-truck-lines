173d2467e71bad36df477a3482e6dcf2cae19593
asfand687 <asfandyar687@gmail.com>
2023-02-01 00:51:34 +0500

    increased height for mobile screens

--- src/sections/HomeStatsSection.jsx
+++ src/sections/HomeStatsSection.jsx
@@ -5,25 +5,25 @@ import { FaMoneyCheckAlt, FaWeightHanging } from 'react-icons/fa'
 
 const HomeStatsSection = () => {
   return (
-    <section className="relative h-[700px]">
-      <div className="absolute top-0 left-0 w-full h-[700px] z-10 bg-black opacity-80" />
-      <div className="absolute left-0 z-0 top-0 w-full h-[700px]  bg-blue-500 ">
-        <img src={Trucks} alt="Trucks" className="w-full h-[700px] object-cover" />
+    <section className="relative h-[1000px] md:h-[700px]">
+      <div className="absolute top-0 left-0 w-full h-[1000px] md:h-[700px] z-10 bg-black opacity-80" />
+      <div className="absolute left-0 z-0 top-0 w-full h-[1000px] md:h-[700px]  bg-blue-500 ">
+        <img src={Trucks} alt="Trucks" className="w-full h-[1000px] md:h-[700px] object-cover" />
       </div>
-      <div className="relative z-20 w-full max-w-5xl mx-auto flex flex-col justify-center font-bold pt-16">
+      <div className="relative px-4 z-20 w-full max-w-5xl mx-auto flex flex-col justify-center font-bold pt-16">
         <div className="h-[6px] w-36 bg-yellow-600 mb-6" />
         <h2 className="text-5xl text-white">
           Our Milestones
         </h2>
-        <div className="flex justify-center items-center gap-x-16 pt-24 py-16">
-          <article className="flex px-8 flex-col items-center text-center border-r-[0.25px] border-gray-500">
+        <div className="flex flex-col md:flex-row space-y-10 md:space-y-0 justify-center items-center md:gap-x-16 pt-24 py-16">
+          <article className="flex px-8 flex-col items-center text-center md:border-r-[0.25px] border-gray-500">
             <TbTruckDelivery className="text-yellow-600 text-8xl pb-4" />
             <div className="text-white">
               <h2 className=" pb-2 text-6xl font-bold text-white">5000+</h2>
               <p className="text-2xl">Freight Loads Moved</p>
             </div>
           </article>
-          <article className="flex px-8 flex-col items-center text-center border-r-[0.25px] border-gray-500">
+          <article className="flex px-8 flex-col items-center text-center md:border-r-[0.25px] border-gray-500">
             <FaMoneyCheckAlt className="text-yellow-600 text-8xl pb-4" />
             <div className="text-white">
               <h2 className=" pb-2 text-6xl font-bold text-white">$7500 +</h2>
