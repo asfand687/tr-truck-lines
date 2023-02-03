d4d76bc9411f3ab65dc949baa716db0213aa3665
asfand687 <asfandyar687@gmail.com>
2023-02-03 20:09:09 +0500

    added overflow hidden to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [showDrawer, setShowDrawer] = useState(false)
   return (
     <nav>
-      <div className="w-full px-4 max-w-7xl mx-auto py-2 flex items-center justify-between">
+      <div className="w-full px-4 max-w-7xl mx-auto py-2 flex items-center justify-between overflow-hidden">
         <img src={Logo} className="w-32" alt="Logo"/>
         <aside className="hidden lg:flex items-center space-x-12">
           <ul className="flex space-x-8">
