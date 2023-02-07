d194b446a0e4fa32cc922fcf43b5f948e43797c9
asfand687 <asfandyar687@gmail.com>
2023-02-07 19:59:28 +0500

    added new icons to service card

--- src/sections/HomeServices.jsx
+++ src/sections/HomeServices.jsx
@@ -3,6 +3,8 @@ import Dispatch from '../assets/dispatching.png'
 import Billing from '../assets/bililng.png'
 import Paperwork from '../assets/paperwork.png'
 import IFTA from '../assets/ifta.png'
+import { HiPlus } from 'react-icons/hi'
+import { FiChevronRight } from 'react-icons/fi'
 
 const HomeServices = () => {
   return (
@@ -37,6 +39,22 @@ const HomeServices = () => {
               <p className="text-sm text-gray-600 w-full lg:max-w-[200px] px-6 md:px-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolorum, tempore molestiae omnis excepturi nam.</p>
             </div>
             <img className="md:absolute -right-0 -bottom-0 w-[250px]" src={IFTA} alt="IFTA" />
+          </article>
+        </div>
+        <div>
+          <article className={`group relative w-[500px] h-[200px] overflow-hidden bg-service-truckdispatch p-10 text-white font-semibold`}>
+            <div className="relative z-[9]">
+              <h2 className="text-4xl">01</h2>
+              <h3 className="text-3xl">Truck Dispatching</h3>
+            </div>
+            <div className="absolute z-[8] transition- ease-linear duration-200 group-hover:opacity-0 left-0 top-0 w-full h-full bg-[#1E73FB]">
+            </div>
+            <div className="absolute top-20 right-10 z-[9]">
+              <HiPlus className="text-[#4DDE82] text-4xl"/>
+            </div>
+            <div className="z-10 flex justify-center items-center transition-all ease-in duration-200 position absolute top-0 -right-24 group-hover:right-0 bg-[#4DDE82] w-24 h-full">
+              <FiChevronRight className="text-[#EAF3F4] text-3xl"/>
+            </div>
           </article>
         </div>
       </div>
