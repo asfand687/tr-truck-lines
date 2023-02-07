c218af69fb5544aceeb5fb9884a00202b5c547e1
asfand687 <asfandyar687@gmail.com>
2023-02-07 23:41:50 +0500

    removed padding bottom

--- src/sections/HomeLocationSection.jsx
+++ src/sections/HomeLocationSection.jsx
@@ -3,8 +3,8 @@ import Map from '../assets/map.png'
 
 const HomeLocationSection = () => {
   return (
-    <section data-aos="flip-up" className="flex flex-col md:flex-row md:items-center py-8 md:py-20 px-4 text-center md:text-left gap-y-10">
-      <article className='flex-1 flex justify-end'>
+    <section data-aos="flip-up" className="flex flex-col md:flex-row md:items-center py-8 md:pb-0 md:pt-20 text-center md:text-left gap-y-10 md:gap-y-0">
+      <article className='flex-1 flex justify-end px-4'>
         <div className="w-full lg:w-[70%] space-y-6">
           <h2 className="text-3xl font-semibold">
             Located in Avnel NJ
