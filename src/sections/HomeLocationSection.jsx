da72da037f145f95ea168c5c8420a2b66bcfe0bd
asfand687 <asfandyar687@gmail.com>
2023-02-08 01:02:48 +0500

    removed horizontal padding

--- src/sections/HomeLocationSection.jsx
+++ src/sections/HomeLocationSection.jsx
@@ -4,7 +4,7 @@ import Map from '../assets/map.png'
 const HomeLocationSection = () => {
   return (
     <section data-aos="flip-up" className="flex flex-col md:flex-row md:items-center py-8 md:pb-0 md:pt-20 text-center md:text-left gap-y-10 md:gap-y-0">
-      <article className='flex-1 flex justify-end px-4'>
+      <article className='flex-1 flex justify-end px-4 md:px-0'>
         <div className="w-full lg:w-[70%] space-y-6">
           <h2 className="text-3xl font-semibold">
             Located in Avnel NJ
