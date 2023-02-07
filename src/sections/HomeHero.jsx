a64ef8ca01548bf6d55d9fc7cb5305dd0bae70cf
asfand687 <asfandyar687@gmail.com>
2023-02-07 23:41:36 +0500

    changed text color

--- src/sections/HomeHero.jsx
+++ src/sections/HomeHero.jsx
@@ -6,9 +6,9 @@ const HomeHero = () => {
   return (
     <section className="h-screen md:h-[calc(100vh-65px)]">
       <div className="relative">
-        <div className="fixed left-10 bottom-10 bg-gray-800 rounded-full z-50 w-16 h-16 flex justify-center items-center animate-pulse">
-          <AiTwotonePhone className="relative z-20 text-5xl text-yellow-400" />
-          <div className="absolute z-10 w-20 h-20 rounded-full bg-gray-500 animate-ping"></div>
+        <div className="fixed left-10 bottom-10 bg-primary rounded-full z-50 w-16 h-16 flex justify-center items-center animate-pulse">
+          <AiTwotonePhone className="relative z-20 text-5xl text-secondary" />
+          <div className="absolute z-10 w-20 h-20 rounded-full bg-accent animate-ping"></div>
         </div>
         <div className="absolute w-full  h-screen md:h-[calc(100vh-65px)] z-20 flex flex-col md:flex-row justify-center items-center text-white gap-x-16">
           <article className="mt-[200px] px-4 pb-10 md:mt-0 md:pb-0">
@@ -16,7 +16,7 @@ const HomeHero = () => {
               <div className="mx-auto md:mx-0 h-[6px] w-36 bg-yellow-600 mb-6"></div>
               <h2 className="text-4xl lg:text-7xl font-bold lg:max-w-[500px]">Freight Dispatch Service</h2>
               <p className="py-4">Full dispatch services for trucks. We provide high quality dispatching services with the best industry standards and additional realted services.</p>
-              <button className="bg-black text-xl text-yellow-400 rounded w-full max-w-[280px] py-3 font-semibold">
+              <button className="bg-primary text-xl text-light rounded w-full max-w-[280px] py-3 font-semibold">
                 Get a Dispatcher
               </button>
             </div>
@@ -39,7 +39,7 @@ const HomeHero = () => {
                 <input className="bg-gray-200 rounded outline-none border w-full px-4 py-2" type="email" placeholder="Email Address" />
               </div>
               <div>
-                <input className="py-3 rounded text-xl w-full bg-black text-yellow-400 font-semibold mt-4" type="submit" value="Send Request" />
+                <input className="py-3 rounded text-xl w-full bg-primary text-light font-semibold mt-4" type="submit" value="Send Request" />
               </div>
             </form>
         </article>
