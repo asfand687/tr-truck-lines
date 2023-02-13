3f56aa29b043de601c904cd473757362e0264825
asfand687 <asfandyar687@gmail.com>
2023-02-13 20:54:29 +0500

    added contact form component

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <article data-aos="fade-left" className="bg-white rounded-2xl md:p-10 p-6 md:py-16 border shadow-2xl text-gray-800">
+      <h2 className="font-semibold text-3xl pb-4">
+        Contact Us For Free Setup
+      </h2>
+      <p className="text-gray-600 text-base max-w-[400px] pb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.
+      </p>
+      <form className="space-y-4">
+        <div>
+          <input className="bg-gray-200 rounded outline-none border w-full px-4 py-2" type="text" placeholder="Full Name" />
+        </div>
+        <div>
+          <input className="bg-gray-200 rounded outline-none border w-full px-4 py-2" type="tel" placeholder="Phone Number" />
+        </div>
+        <div>
+          <input className="bg-gray-200 rounded outline-none border w-full px-4 py-2" type="email" placeholder="Email Address" />
+        </div>
+        <div>
+          <input className="py-3 rounded text-xl w-full bg-primary text-light font-semibold mt-4" type="submit" value="Send Request" />
+        </div>
+      </form>
+    </article>
+  )
+}
+
+export default ContactForm
