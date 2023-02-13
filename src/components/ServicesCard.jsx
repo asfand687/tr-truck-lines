b9040798b84940645f6115e462350ecaded33f0c
asfand687 <asfandyar687@gmail.com>
2023-02-13 22:27:55 +0500

    added new animations

--- src/components/ServicesCard.jsx
+++ src/components/ServicesCard.jsx
@@ -4,10 +4,15 @@ import { FiChevronRight } from 'react-icons/fi'
 
 const servicesCard = ({number, title, className, fade}) => {
   return (
-    <article data-aos={fade} className={`group relative overflow-hidden  p-10 text-white font-semibold rounded-lg ${className}`}>
-      <div className="relative z-[9] flex flex-col justify-center h-full">
-        {number && <h2 className="text-3xl md:text-4xl">0{number}</h2>}
-        <h3 className="text-2xl md:text-3xl">{title}</h3>
+    <article data-aos={fade} className={`group relative overflow-hidden text-light font-semibold rounded-lg ${className}`}>
+      <div className="relative z-[9] flex flex-col p-10 justify-center transition-all ease-in duration-300 group-hover:p-4 group-hover:justify-start h-full">
+        <div className="flex flex-col md:group-hover:flex-row md:group-hover:gap-x-2">
+          {number && <h2 className="text-3xl md:text-4xl md:group-hover:text-lg">0{number}</h2>}
+          <h3 className="text-2xl md:text-3xl group-hover:text-base">{title}</h3>
+        </div>
+        <p className="absolute -right-full text-light group-hover:right-28 transition-all ease-linear duration-300 top-4 text-sm max-w-[190px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus quas ratione magni voluptatum, nulla nisi aperiam fugit eius. Repellat voluptates vero odio quisquam eos.
+        </p>
       </div>
       <div className="absolute z-[8] opacity-30 md:opacity-100 transition- ease-linear duration-200 md:group-hover:opacity-0 left-0 top-0 w-full h-full bg-primary">
       </div>
