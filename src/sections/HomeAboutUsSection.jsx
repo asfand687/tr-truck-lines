681af22cc64ac08d3c6f4655b2c5c73013c38761
asfand687 <asfandyar687@gmail.com>
2023-02-13 22:58:55 +0500

    added animations to mission and about us

--- src/sections/HomeAboutUsSection.jsx
+++ src/sections/HomeAboutUsSection.jsx
@@ -5,11 +5,52 @@ import Vision from '../assets/vision.jpg'
 const HomeAboutUsSection = () => {
   return (
     <section>
-      <h2 className="text-4xl font-semibold text-center">About Us</h2>
+      <div className="px-4">
+        <article className="pb-6">
+          <h2 className="text-4xl font-semibold text-center pb-4">About Us</h2>
+          <div className="w-full max-w-4xl mx-auto space-y-3">
+            <p data-aos="fade-right">
+              Welcome to TR Truck Lines, your reliable and efficient freight dispatch solution. Our mission is to provide top-notch logistics services that make shipping simple and stress-free for our customers.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              We understand the complexities and challenges of managing a successful shipment, and that's why we've brought together a team of experts with decades of experience in the industry. Our team works tirelessly to provide customized, cost-effective logistics solutions that meet the unique needs of each of our customers.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              Our services include carrier selection, load planning, route optimization, real-time tracking, documentation handling, and much more. We have established strong partnerships with a network of carriers, allowing us to provide our customers with a wide range of options and the flexibility to choose the right transportation method for their shipment.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              At TR Truck Lines, we are committed to providing the highest level of customer service and satisfaction. Whether you are a small business or a large corporation, we are here to help you manage your shipments with ease and peace of mind.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              We invite you to explore our website and learn more about the services we offer. If you have any questions or would like to discuss your logistics needs, please don't hesitate to contact us. Thank you for choosing TR Truck Lines as your dispatch solution.
+            </p>
+          </div>
+        </article>
+        <article>
+          <h2 className="text-4xl font-semibold text-center pb-4">Our Mission</h2>
+          <div className="w-full max-w-4xl mx-auto space-y-3">
+            <p data-aos="fade-right">
+              At TR Truck Lines, our mission is to revolutionize the logistics industry by providing superior freight dispatch services that simplify shipping and enhance the customer experience.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              We believe that shipping should be effortless and worry-free. That's why we strive to deliver the best possible logistics solutions, combining technology, expertise, and a commitment to customer satisfaction.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              Our goal is to make shipping easier for businesses of all sizes by providing them with access to the resources and tools they need to manage their shipments efficiently and effectively. Whether you are looking to optimize your supply chain, reduce costs, or improve transit times, we have the experience and knowledge to help.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              We are dedicated to providing the highest quality of service to our customers and building long-lasting relationships based on trust, reliability, and excellence. Our team of experienced professionals is here to support you every step of the way, ensuring that your shipments are handled with the utmost care and attention.
+            </p>
+            <p data-aos="fade-right" data-aos-delay="300">
+              At TR Truck Lines, we are passionate about what we do and committed to making a difference in the logistics industry. Join us on our mission to simplify shipping and enhance the customer experience.
+            </p>
+          </div>
+        </article>
+      </div>
       <HomeLocationSection/>
-      <section className="flex flex-col-reverse md:flex-row items-center pb-10">
+      <section className="flex flex-col-reverse md:flex-row pb-10">
         <article className="flex-1">
-          <img className="w-full h-[400px] object-cover" src={Vision} alt="Our Vision"/>
+          <img className="w-full object-cover" src={Vision} alt="Our Vision" />
         </article>
         <article className="flex-1 text-center md:text-left pt-4 space-y-6">
         <div className="w-full space-y-6 px-4 md:pl-14">
@@ -17,9 +58,13 @@ const HomeAboutUsSection = () => {
             Our Vision
           </h2>
             <div className="w-full max-w-[530px] pb-6 md:pb-0">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam quaerat perferendis nam necessitatibus praesentium</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam quaerat perferendis nam necessitatibus praesentium</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam quaerat perferendis nam necessitatibus praesentium</p>
+              <p>
+                At TR Truck Lines, our vision is to be the leading provider of innovative and reliable freight dispatch services, delivering unparalleled value to our customers and stakeholders.
+                We see a future where shipping is seamless, efficient, and accessible to businesses of all sizes. A future where technology, expertise, and customer focus come together to create a logistics experience that exceeds expectations.
+                We strive to empower our customers with the tools and resources they need to manage their shipments with confidence, and to make shipping a competitive advantage for their businesses. Our vision is to help our customers succeed by providing them with the highest quality of service and the most advanced logistics solutions available.
+                We believe in the power of collaboration and teamwork, and we are committed to building strong relationships with our customers, partners, and employees. Our vision is to create a thriving community of logistics professionals who share our passion for excellence and are dedicated to making a positive impact in the industry.
+                At TR Truck Lines, we are passionate about what we do and believe in the power of our vision. Join us as we strive to shape the future of the logistics industry and deliver unparalleled value to our customers.
+              </p>
           </div>
         </div>
         </article>
