baaf1f2da47dfb8dc0c4186deb5f2d3a3cf081e1
asfand687 <asfandyar687@gmail.com>
2023-02-17 15:19:03 +0500

    added testimonials section

--- src/sections/HomeTestimonialsSection.jsx
+++ src/sections/HomeTestimonialsSection.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const HomeTestimonialsSection = () => {
   return (
-    <div>HomeTestimonialsSection</div>
+    <section className="py-20">
+      <h2 className="text-center text-4xl font-semibold pb-12">
+        Here is what our clients say about us
+      </h2>
+    </section>
   )
 }
 
