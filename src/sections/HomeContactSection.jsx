ff75de1209038c2fc49a45ecf6e13eae747fc6e1
asfand687 <asfandyar687@gmail.com>
2023-02-17 15:13:24 +0500

    added additional info

--- src/sections/HomeContactSection.jsx
+++ src/sections/HomeContactSection.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import ContactForm from '../components/ContactForm'
 
 const HomeContactSection = () => {
   return (
-    <div>HomeContactSection</div>
+    <section className="py-20  bg-primary">
+      <div className="max-w-5xl mx-auto flex items-center justify-center gap-x-24">
+        <article className="max-w-xs">
+          <h2 className="text-secondary font-semibold pb-4 text-4xl">Get in touch</h2>
+          <div className="text-accent space-y-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, voluptate!</p>
+            <div>
+              <h3 className="text-secondary text-xl">
+                Phone
+              </h3>
+              <p>+1 (917) 400-1006</p>
+            </div>
+            <div>
+              <h3 className="text-secondary text-xl">
+                Email
+              </h3>
+              <p>connect@trtrucklines.com</p>
+            </div>
+
+            <div>
+              <h3 className="text-secondary text-xl">
+                Address
+              </h3>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, libero? Aperiam aut repellendus nemo fugit?</p>
+            </div>
+          </div>
+        </article>
+        <article>
+          <ContactForm />
+        </article>
+      </div>
+    </section>
   )
 }
 
