56ae48f2b5cdc896f6244aca635f6d86d906632f
asfand687 <asfandyar687@gmail.com>
2023-02-22 18:17:28 +0500

    added testimonials

--- src/sections/HomeTestimonialsSection.jsx
+++ src/sections/HomeTestimonialsSection.jsx
@@ -5,6 +5,33 @@ import Testimonial01 from '../assets/testimonial-01.png'
 import Testimonial02 from '../assets/testimonial-02.png'
 import Testimonial03 from '../assets/testimonial-03.png'
 
+const testimonials = [
+  {
+    name: "Jason Anderson",
+    business: "Rapid Transport Inc.",
+    mcNumber: 93874,
+    image: Testimonial01,
+    title: "Reliable, Professional, and Efficient Services",
+    review: "I've been working with TR Truck Lines for several months now and I have to say, they are the best in the business! They are always reliable, professional, and efficient. They have helped me streamline my transportation operations and I couldn't be happier with their services. I highly recommend them to anyone in the industry."
+  },
+  {
+    name: "Robert Thompson",
+    business: "Pacific Coast Trucking",
+    mcNumber: 54281,
+    image: Testimonial02,
+    title: "Reliable, Professional, and Efficient Services",
+    review: "I was hesitant to use a truck dispatching company at first, but I'm so glad I found TR Truck Lines. Their team is knowledgeable, friendly, and always willing to go the extra mile to make sure everything runs smoothly. They have saved me so much time and money, and I couldn't be happier with their services. They are truly a top-notch company!"
+  },
+  {
+    name: "Michael Davis",
+    business: "Mountain View Logistics",
+    mcNumber: 78432,
+    image: Testimonial03,
+    title: "Reliable, Professional, and Efficient Services",
+    review: "As a small business owner, I was struggling to keep up with my transportation needs. But ever since I started working with TR Truck Lines, my stress levels have gone down significantly. They are always prompt, professional, and communicative. They make sure all of my deliveries are on time and in great condition. I highly recommend them to anyone in need of reliable transportation services!"
+  }
+]
+
 const HomeTestimonialsSection = () => {
   const [currentSlide, setCurrentSlide] = useState(0)
   return (
@@ -28,42 +55,31 @@ const HomeTestimonialsSection = () => {
           </div>
           <div className="relative z-10 overflow-hidden">
             <div className={`flex rounded-xl transition-transform ease-in-out duration-300 ${currentSlide === 0 ? "translate-x-0" : currentSlide === 1 ? "-translate-x-[360px] md:-translate-x-[630px]" : "-translate-x-[720px] md:-translate-x-[1260px]"}`}>
-              <article className="min-w-[360px] md:min-w-[630px] text-center bg-primary p-6 rounded-lg">
-                <div className="space-y-3">
-                  <img className="mx-auto w-20 h-20" src={Testimonial01} alt="Jack Roper" />
-                  <h4 className="text-accent">Jack Roper</h4>
-                </div>
-                <h3 className="text-2xl font-semibold pb-4 text-secondary">
-                  It was a very good experience
-                </h3>
-                <p className="text-accent leading-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. urpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu
-                </p>
-              </article>
-              <article className="min-w-[360px] md:min-w-[630px] text-center bg-primary p-6 rounded-lg">
-                <div className="space-y-3">
-                  <img className="mx-auto w-20 h-20" src={Testimonial02} alt="Richard Darcy" />
-                  <h4 className="text-accent">Richard Darcy</h4>
-                </div>
-                <h3 className="text-2xl font-semibold pb-4 text-secondary">
-                  It was a nice experience
-                </h3>
-                <p className="text-accent leading-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. urpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu
-                </p>
-              </article>
-              <article className="min-w-[360px] md:min-w-[630px] text-center bg-primary p-6 rounded-lg">
-                <div className="space-y-3">
-                  <img className="mx-auto w-20 h-20" src={Testimonial03} alt="Gavin Mcbride" />
-                  <h4 className="text-accent">Gavin Mcbride</h4>
-                </div>
-                <h3 className="text-2xl font-semibold pb-4 text-secondary">
-                  It was awesome experience
-                </h3>
-                <p className="text-accent leading-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu. Elementum felis magna pretium in tincidunt. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. urpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim arcu
-                </p>
-              </article>
+              {
+                testimonials.map(testimonial => (
+                  <article
+                    key={testimonial.name}
+                    className="min-w-[360px] md:min-w-[630px] text-center bg-primary p-6 rounded-lg"
+                  >
+                    <div className="text-accent">
+                      <div className="space-y-3">
+                        <img className="mx-auto w-20 h-20" src={testimonial.image} alt={testimonial.name} />
+                        <h4 className="text-lg">{testimonial.name}</h4>
+                      </div>
+                      <div className="text-sm">
+                        <h5>{testimonial.business}</h5>
+                        <h5><strong>MC # {testimonial.mcNumber}</strong></h5>
+                      </div>
+                    </div>
+                    <h3 className="text-2xl font-semibold pb-2 text-secondary">
+                      {testimonial.title}
+                    </h3>
+                    <p className="text-accent leading-6">
+                      {testimonial.review}
+                    </p>
+                  </article>
+                ))
+              }
             </div>
           </div>
         </section>
