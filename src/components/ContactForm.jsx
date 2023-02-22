658209527f9c4ee345c2b9d6730cd2114b136f5d
asfand687 <asfandyar687@gmail.com>
2023-02-22 11:59:36 +0500

    added ring transition on focus

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactForm = () => {
   return (
-    <article data-aos="fade-left" className="bg-white rounded-2xl md:p-10 p-6 md:py-16 border shadow-2xl text-gray-800">
+    <article data-aos="fade-left" className="bg-white rounded-2xl md:p-10 p-6 py-16 border shadow-2xl text-gray-800">
       <h2 className="text-primary font-semibold text-3xl pb-4">
         Contact Us For Free Setup
       </h2>
@@ -11,13 +11,13 @@ const ContactForm = () => {
       </p>
       <form className="space-y-4">
         <div>
-          <input className="bg-gray-200 rounded outline-none border w-full px-4 py-2" type="text" placeholder="Full Name" />
+          <input className="bg-gray-100 rounded outline-none border w-full px-4 py-2 ring-1 focus:ring-primary transition-all ease-out duration-300" type="text" placeholder="Full Name" />
         </div>
         <div>
-          <input className="bg-gray-200 rounded outline-none border w-full px-4 py-2" type="tel" placeholder="Phone Number" />
+          <input className="bg-gray-100 rounded outline-none border w-full px-4 py-2 ring-1 focus:ring-primary transition-all ease-out duration-300" type="tel" placeholder="Phone Number" />
         </div>
         <div>
-          <input className="bg-gray-200 rounded outline-none border w-full px-4 py-2" type="email" placeholder="Email Address" />
+          <input className="bg-gray-100 rounded outline-none border w-full px-4 py-2 ring-1 focus:ring-primary transition-all ease-out duration-300" type="email" placeholder="Email Address" />
         </div>
         <div>
           <input className="py-3 rounded text-xl w-full bg-primary text-light font-semibold mt-4" type="submit" value="Send Request" />
