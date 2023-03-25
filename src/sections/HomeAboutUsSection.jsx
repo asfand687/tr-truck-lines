836ea0c201144de8b03dc5f44e1ef90d8c28799e
asfand687 <asfandyar687@gmail.com>
2023-03-26 01:35:48 +0500

    fixed logistics to freight

--- src/sections/HomeAboutUsSection.jsx
+++ src/sections/HomeAboutUsSection.jsx
@@ -10,7 +10,7 @@ const HomeAboutUsSection = () => {
           <h2 className="text-4xl font-semibold text-center pb-4">About Us</h2>
           <div className="w-full max-w-4xl mx-auto space-y-3">
             <p data-aos="fade-right">
-              TR Truck Lines is a logistics company that offers reliable and efficient freight dispatch services. They have an experienced team that provides customized logistics solutions to meet the unique needs of each customer. Their services include carrier selection, load planning, route optimization, real-time tracking, and more. They are committed to providing the highest level of customer service and satisfaction.
+              TR Truck Lines is a freight dispatch company that offers reliable and efficient freight dispatch services. They have an experienced team that provides customized freight solutions to meet the unique needs of each carrier. Their services include carrier selection, load planning, route optimization, real-time tracking, and more. They are committed to providing the highest level of customer service and satisfaction.
             </p>
           </div>
         </article>
