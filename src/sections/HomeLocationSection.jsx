46e747a5859ab85c4ab6ad408b4325656f6d6232
asfand687 <asfandyar687@gmail.com>
2023-03-26 01:41:24 +0500

    removed dummy text

--- src/sections/HomeLocationSection.jsx
+++ src/sections/HomeLocationSection.jsx
@@ -3,21 +3,12 @@ import Map from '../assets/map.png'
 
 const HomeLocationSection = () => {
   return (
-    <section data-aos="flip-up" className="flex flex-col md:flex-row md:items-center py-8 md:pb-0 md:pt-20 text-center md:text-left gap-y-10 md:gap-y-0">
-      <article className='flex-1 flex justify-end px-4 md:px-0'>
-        <div className="w-full lg:w-[70%] space-y-6">
-          <h2 className="text-3xl font-semibold">
-            Located in Avnel NJ
-          </h2>
-          <div>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam quaerat perferendis nam necessitatibus praesentium</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam quaerat perferendis nam necessitatibus praesentium</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt numquam quaerat perferendis nam necessitatibus praesentium</p>
-          </div>
-        </div>
-      </article>
+    <section data-aos="flip-up" className="flex flex-col md:pb-0 md:pt-20 text-center md:text-left gap-y-10 md:gap-y-0">
+      <h2 className="text-3xl text-center font-semibold pb-6">
+        Located in Avnel NJ
+      </h2>
       <article className='flex-1'>
-        <img className="w-full h-[400px] object-cover" src={Map} alt="Map" />
+        <img className="w-full h-[500px] object-cover" src={Map} alt="Map" />
       </article>
     </section>
   )
