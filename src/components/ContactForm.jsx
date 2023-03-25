21bf6d6c56f78898cbfa5b8ba477ccc896d0fc09
asfand687 <asfandyar687@gmail.com>
2023-03-26 02:03:27 +0500

    removed lorem ipsum

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,9 +6,7 @@ const ContactForm = () => {
       <h2 className="text-primary font-semibold text-3xl pb-4">
         Contact Us For Free Setup
       </h2>
-      <p className="text-accent text-base max-w-[400px] pb-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.
-      </p>
+
       <form className="space-y-4">
         <div>
           <input className="bg-gray-100 rounded outline-none border w-full px-4 py-2 ring-1 focus:ring-primary transition-all ease-out duration-300" type="text" placeholder="Full Name" />
