60dcc1b5e5672b8e814127186260116f18f5b064
asfand687 <asfandyar687@gmail.com>
2023-05-02 22:28:05 +0500

    added scroll links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,72 @@ import React, { useState } from 'react'
 import { ReactComponent as Logo } from '../assets/logo-new.svg'
 import Drawer from '../components/Drawer'
 import Hamburger from '../assets/hamburger.svg'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   const [showDrawer, setShowDrawer] = useState(false)
   return (
-    <nav>
+    <nav className="fixed top-0 left-0 w-full z-50 bg-white">
       <div className="w-full px-4 max-w-7xl mx-auto py-2 flex items-center justify-between overflow-hidden">
-        <Logo className="w-12 h-16"/>
+        <Link
+          className="cursor-pointer"
+          to="home"
+          smooth={true}
+          duration={500}
+        >
+          <Logo className="w-12 h-16"/>
+        </Link>
         <aside className="hidden lg:flex items-center space-x-12">
           <ul className="flex space-x-8">
-            <li>About Us</li>
-            <li>Our Vision</li>
-            <li>Pricing</li>
+            <li>
+              <Link
+                activeClass='nav-active-link'
+                offset={-155}
+                to="about"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer"
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass='font-semibold text-primary'
+                offset={-165}
+                to="vision"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer"
+                >
+                  Our Vision
+                </Link>
+            </li>
+            <li>
+              <Link
+                activeClass='font-semibold text-primary'
+                offset={-145}
+                to="pricing"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer"
+                >
+                  Pricing
+              </Link>
+            </li>
           </ul>
-          <button className="rounded font-semibold px-6 py-2 bg-primary text-light">Get a Dispatcher</button>
+          <Link
+            activeClass='font-semibold text-primary'
+            offset={-115}
+            to="contact"
+            smooth={true}
+            duration={500}
+            className="cursor-pointer"
+            >
+            <button className="rounded font-semibold px-6 py-2 bg-primary text-light">
+              Get a Dispatcher
+            </button>
+          </Link>
         </aside>
       </div>
       <img onClick={() => setShowDrawer(!showDrawer)} className='md:hidden absolute top-6 right-4' src={Hamburger} alt="Hamburger" />
