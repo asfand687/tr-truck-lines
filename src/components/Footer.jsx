56c6801facd9a69323e8af294b69b6057898fa28
asfand687 <asfandyar687@gmail.com>
2023-05-02 22:27:55 +0500

    added logo to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import Logo from '../assets/logo.svg'
 import Facebook from '../assets/facebook.png'
 import Linkedin from '../assets/linkedin.png'
 import Twitter from '../assets/twitter.png'
 import Envelope from '../assets/envelope.svg'
 import Phone from '../assets/phone.svg'
 import { FaInstagram } from 'react-icons/fa'
+import { ReactComponent as Logo } from '../assets/logo-new.svg'
 
 const Footer = () => {
   return (
     <footer className="border-t">
-      <div className="w-full max-w-7xl mx-auto flex flex-col items-center md:flex-row md:items-start gap-y-4 py-20">
-        <img className="w-40" src={Logo} alt="Logo" />
+      <div className="w-full max-w-7xl mx-auto flex flex-col items-center md:flex-row md:items-start gap-y-4 py-20 px-4">
+        <Logo className="w-12 h-16"/>
         <article className="flex-1 gap-y-4">
           <div className="flex flex-col items-center md:items-start md:flex-row md:justify-center lg:space-x-16 pb-4 text-center md:text-left">
             <h2>Home</h2>
